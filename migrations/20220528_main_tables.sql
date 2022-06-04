bcd5a40bb5ec6b39185169040eed9dec2e7b7327
JonathanElejalde <jonathanelejalde@hotmail.com>
2022-06-04 16:44:00 -0500

    Update database schema

--- migrations/20220528_main_tables.sql
+++ migrations/20220528_main_tables.sql
@@ -3,9 +3,12 @@
 #
 
 CREATE TABLE `users` (
-  `user_id` INT unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` varchar(50) NOT NULL UNIQUE,
   `email` varchar(250) NOT NULL UNIQUE,
   `password` varchar(255) NOT NULL,
+  `first_name` varchar(100),
+  `last_name` varchar(100),
+  `birth_date` date,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
 
@@ -17,7 +20,7 @@ CREATE TABLE `users` (
 CREATE TABLE `categories` (
   `category_id` INT unsigned NOT NULL AUTO_INCREMENT,
   `category_name` varchar(255) NOT NULL,
-  `user_id` INT unsigned NOT NULL,
+  `user_id` varchar(50) NOT NULL,
   PRIMARY KEY (`category_id`),
   FOREIGN KEY (`user_id`) REFERENCES users(`user_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4;
@@ -28,7 +31,7 @@ CREATE TABLE `categories` (
 
 CREATE TABLE `projects` (
   `project_id` INT unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` INT unsigned NOT NULL,
+  `user_id` varchar(50) NOT NULL,
   `category_id` INT unsigned NOT NULL,
   `project_name` varchar(250) NOT NULL,
   `start` date,
@@ -46,7 +49,7 @@ CREATE TABLE `projects` (
 
 CREATE TABLE `pomodoros` (
   `pomodoro_id` INT unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` INT unsigned NOT NULL,
+  `user_id` varchar(50) NOT NULL,
   `category_id` INT unsigned NOT NULL,
   `project_id` INT unsigned NOT NULL,
   `duration` INT unsigned NOT NULL,
@@ -64,7 +67,7 @@ CREATE TABLE `pomodoros` (
 
 CREATE TABLE `recalls` (
   `recall_id` INT unsigned NOT NULL AUTO_INCREMENT,
-  `user_id` INT unsigned NOT NULL,
+  `user_id` varchar(50) NOT NULL,
   `project_name` varchar(255) NOT NULL,
   `recall` text,
   `recall_title` varchar(255) NOT NULL,
