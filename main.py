1527b398a15a06553d63f6c2c49dbc958e392d63
JonathanElejalde <jonathanelejalde@hotmail.com>
2022-06-20 19:05:15 -0500

    Delete sample code

--- main.py
+++ main.py
@@ -15,27 +15,6 @@ app.include_router(pomodoros.router)
 app.include_router(recall_projects.router)
 app.include_router(recalls.router)
 
-
-# from fastapi import FastAPI, Request
-# from fastapi.responses import HTMLResponse
-# from fastapi.staticfiles import StaticFiles
-# from fastapi.templating import Jinja2Templates
-
-# app = FastAPI()
-
-# app.mount("/static", StaticFiles(directory="static"), name="static")
-
-
-# templates = Jinja2Templates(directory="templates")
-
-
-# @app.get("/items/{id}", response_class=HTMLResponse)
-# async def read_item(request: Request, id: str):
-#     print(request)
-#     return templates.TemplateResponse("item.html", {"request": request, "id": id})
-
-
-
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", reload=True)
