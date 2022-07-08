4c5f705e5028017579289341a9ca8ddcc2f2855d
JonathanElejalde <jonathanelejalde@hotmail.com>
2022-07-07 21:33:58 -0500

    Add /projects page

--- routers/projects.py
+++ routers/projects.py
@@ -1,6 +1,7 @@
 from datetime import datetime
+from typing import Optional
 
-from fastapi import APIRouter, status, Depends, HTTPException, Request
+from fastapi import APIRouter, status, Depends, HTTPException, Request, Header, Form, Query, Path
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
@@ -38,42 +39,12 @@ def create_project(project:Project, current_user:ResponseUser = Depends(get_curr
 @router.get(
     path="/",
     status_code=status.HTTP_200_OK,
-    summary="Get projects",
-    response_class=HTMLResponse
+    response_class=HTMLResponse,
+    include_in_schema=False
 )
-def get_projects(request: Request, category_id:int, current_user:ResponseUser = Depends(get_current_user)):
-    user_id = current_user['user_id']
-    values = (user_id, category_id)
-    df = q.get_projects(values)
-    if df.empty:
-        return '<select id="project-names" name="project_id"></select>'
-    projects = df.to_dict('records')
-    context = {
-        'request': request,
-        'projects': projects
-    }
+def get_projects(request: Request, current_user:ResponseUser = Depends(get_current_user)):
+    return templates.TemplateResponse("/general_pages/projects.html", {"request": request})
 
-    return templates.TemplateResponse("/components/projects.html", context=context)
-    
-
-@router.get(
-    path="/{project_id}",
-    response_model=ProjectResponse,
-    status_code=status.HTTP_200_OK,
-    summary="Get project"
-)
-def get_project(category_id: int, project_id:int, current_user:ResponseUser = Depends(get_current_user)):
-    user_id = current_user['user_id']
-    values = (user_id, category_id, project_id)
-    df = q.get_projects(values, project_id)
-    
-    if df.empty:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="This project does not exists"
-        )
-    project = df.to_dict('records')[0]
-
-    return project
 
 
 @router.put(
@@ -114,48 +85,97 @@ def update_project(category_id:int, project_id:int, current_user:ResponseUser = 
 
 
 @router.put(
-    path="/{project_id}/name",
+    path="/name/{project_id}",
     status_code=status.HTTP_200_OK,
     summary="Cancel a project"
 )
-def update_project(category_id:int, project_id:int, new_project_name:str, current_user:ResponseUser = Depends(get_current_user)):
+def update_project(request: Request, project_id:int = Path(...,), new_project_name:str = Form(...), current_user:ResponseUser = Depends(get_current_user)):
     user_id = current_user['user_id']
-    values = (new_project_name, category_id, project_id, user_id)
+    values = (new_project_name, project_id, user_id)
     query = q.update_project('project_name')
     
     # Execute query
     DB.execute_query(query, values)
 
-    return {
-        'Detail': f"The project_name of project:{project_id} has been updated to {new_project_name}"
+    # Get project with new data
+    values = [user_id, project_id]
+    df = q.get_projects(values, project_id=project_id)
+
+    df.fillna("", inplace=True)
+    project = df.to_dict('records')[0]
+    context = {
+        "request": request,
+        "project": project
     }
+    
+    return templates.TemplateResponse('components/project_edited.html', context=context)
 
 
 @router.delete(
     path="/{project_id}",
     status_code=status.HTTP_200_OK,
-    summary="Delete project"
+    summary="Delete project",
+    response_class=HTMLResponse
 )
-def delete_project(category_id:int, project_id:int, current_user:ResponseUser = Depends(get_current_user)):
+def delete_project(request: Request, project_id:int = Path(...), current_user:ResponseUser = Depends(get_current_user)):
     user_id = current_user['user_id']
-    values = (category_id, project_id, user_id)
+    values = (project_id, user_id)
     query = q.delete_project()
     
     # Execute query
     DB.execute_query(query, values)
 
-    return {
-        'Detail': f"Project {project_id} was deleted"
+    return "<tr></tr>"
+
+@router.get(
+    path="/names",
+    status_code=status.HTTP_200_OK,
+    summary="Get projects",
+    response_class=HTMLResponse
+)
+def get_projects_names(request: Request, category_id:int = Query(...), current_user:ResponseUser = Depends(get_current_user)):
+    user_id = current_user['user_id']
+    values = (user_id, category_id)
+    df = q.get_projects(values, category_id=category_id)
+    if df.empty:
+        return '<select id="project-names" name="project_id"></select>'
+    df.fillna("", inplace=True)
+    projects = df.to_dict('records')
+    context = {
+        'request': request,
+        'projects': projects
     }
+    current_url = request.headers.get('hx-current-url')
+    current_url = current_url.split("/")
+    current_url = [ele for ele in current_url if ele.strip()]
+    current_url = current_url[-1]
 
+    if current_url == "pomodoro":
+        return templates.TemplateResponse("/components/projects.html", context=context)
+    elif current_url == "projects":
+        return templates.TemplateResponse("/components/projects_table.html", context=context)
+    else:
+        raise ValueError(f"Current url is {current_url}, it does not match pomodoro or projects")
+    
 
+@router.get(
+    path="/edit/{project_id}",
+    include_in_schema=False,
+    response_class=HTMLResponse
+)
+def edit_recall_project(request:Request, project_id:int = Path(...,), 
+    current_user:ResponseUser = Depends(get_current_user)
+    ):
+    user_id = current_user['user_id']
+    values = [user_id, project_id]
+    df = q.get_projects(values, project_id=project_id)
 
-# @router.delete(
-#     path="/{project_ids}",
-#     response_model=list[Project],
-#     status_code=status.HTTP_200_OK,
-#     summary="Delete project"
-# )
-# def delete_projects():
-#     pass
+    df.fillna("", inplace=True)
+    project = df.to_dict('records')[0]
+    context = {
+        "request": request,
+        "project": project
+    }
+    
+    return templates.TemplateResponse('components/edit_project.html', context=context)
 
